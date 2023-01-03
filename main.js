d4a3549d9b1a5194bc0961f1b070dabe5cbe1f5c
femipraize <felixogbangwor@gmail.com>
2023-01-03 12:58:45 +0100

    Vanila JS

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 console.log('Hello World');
 console.error('This is an error');
 console.warn('this is a warning');
+
+// Data types
+// String, Number, Boolean, null, undefine
+
+const name = 'John';
+const age = 30;
+const rating = 4.6;
+const isCool = true;
+const x = null;
+const y = undefined;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
