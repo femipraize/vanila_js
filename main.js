061d59b565922708d49c921020fb165016e0dd90
femipraize <felixogbangwor@gmail.com>
2023-01-03 13:10:49 +0100

    update

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ console.error('This is an error');
 console.warn('this is a warning');
 
 // Data types
+
+// Primitive data types
 // String, Number, Boolean, null, undefine
 
 const name = 'John';
@@ -11,6 +13,7 @@ const rating = 4.6;
 const isCool = true;
 const x = null;
 const y = undefined;
+let z;
 
 console.log(typeof name);
 console.log(typeof age);
@@ -18,3 +21,11 @@ console.log(typeof rating);
 console.log(typeof isCool);
 console.log(typeof x);
 console.log(typeof y);
+console.log(typeof z);
+
+// Concatination
+console.log('My name is ' +name+ 'and i am ' +age)
+//ES6 or Es2015
+//template string/ template leterals
+const hello = `My name is ${name} and i am ${age}`;
+console.log(hello);
