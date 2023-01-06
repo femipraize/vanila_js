14650dffdc4a0ad3b2fa0ed66b001f881e3e1f63
femipraize <felixogbangwor@gmail.com>
2023-01-06 12:33:23 +0100

    events listener + validation

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onsubmit);
+
+function onsubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(()=> msg.remove(), 3000);
+    } else {
+       const li = document.createElement('li');
+       li.appendChild(document.createTextNode(`${nameInput} : ${emailInput}`));
+
+       userList.appendChild(li);
+
+       //Clear fields
+       nameInput.value = '';
+       emailInput.value ='';
+    }
+}
+
+// Targeting element (# for targetting id, . for targetting class)
+
+
+
+
+
+
+
+
+
 // Selector
 // Single element selector
 // console.log(document.getElementById('my=form'));
@@ -25,15 +64,15 @@
 //btn.style.background = 'red';
 
 // Working with events or event listner
-const btn = document.querySelector('.btn');
+//const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', (e) => {
+//btn.addEventListener('mouseover', (e) => {
     // This is to prevent default behavior
-    e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
-});
+  //  e.preventDefault();
+  //  document.querySelector('#my-form').style.background = '#ccc';
+  //  document.querySelector('body').classList.add('bg-dark');
+  //  document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+//});
 /*
 console.log('Hello World');
 console.error('This is an error');
